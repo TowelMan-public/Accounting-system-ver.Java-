f79631a2ebfbbe1779bc69ef5c22d55527e24fba
odagiritomoya <60881933+odagiritomoya@users.noreply.github.com>
2020-11-28 23:11:01 +0900

    Update LoginMapper.java

--- ver.0/main/java/com/example/demo/LoginMapper.java
+++ ver.0/main/java/com/example/demo/LoginMapper.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 @Mapper
 public interface LoginMapper {
-	LoginForm selectById(String username);
+	LoginForm selectById(@Param("username") String username);
 }
